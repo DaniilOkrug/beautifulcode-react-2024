95b818329dbb42510cb658a6f08fd17a9684730d
Daniil Okrug <d.okrug@gazprom-auto.ru>
2024-10-22 19:15:41 +0300

    feat(shadcn): integrated shadcn and tailwind

--- src/app/appEntry.tsx
+++ src/app/appEntry.tsx
@@ -1,7 +1,7 @@
+import { HomePage } from '@/pages/home';
+import '@/shared/index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HomePage } from '../pages/home';
-import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,3 +1,9 @@
 export const HomePage = () => {
-  return <>HomePage</>;
+  return (
+    <div>
+      <h1>Budget manager</h1>
+
+      <p>Balance: $0.00</p>
+    </div>
+  );
 };
