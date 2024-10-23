32df17baf3ca676dcbc1e7f623a9fe829dcccb92
Daniil Okrug <d.okrug@gazprom-auto.ru>
2024-10-23 18:20:12 +0300

    feat(Table): added Table

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -7,4 +7,5 @@ export * from './Separator/Separator';
 export * from './Sheet/Sheet';
 export * from './Sidebar/Sidebar';
 export * from './Skeleton/Skeleton';
+export * from './Table/Table';
 export * from './Tooltip/Tooltip';
