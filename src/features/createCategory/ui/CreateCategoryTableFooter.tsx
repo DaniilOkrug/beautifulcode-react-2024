faa6f522e0333866394cc1e9c8922210f2e95aab
Daniil Okrug <d.okrug@gazprom-auto.ru>
2024-10-24 20:33:49 +0300

    refactor(CreateCategoryTableFooter): reformat

--- src/features/createCategory/ui/CreateCategoryTableFooter.tsx
+++ src/features/createCategory/ui/CreateCategoryTableFooter.tsx
@@ -9,37 +9,37 @@ import {
   TableFooter,
   TableRow,
 } from '@/shared/ui';
-import {useFormContext} from 'react-hook-form';
-import type {CreateCategoryFormScheme} from '../lib/CreateCategoryFormScheme';
+import { useFormContext } from 'react-hook-form';
+import type { CreateCategoryFormScheme } from '../lib/CreateCategoryFormScheme';
 
 export const CreateCategoryTableFooter = () => {
-    const {control} = useFormContext<CreateCategoryFormScheme>();
+  const { control } = useFormContext<CreateCategoryFormScheme>();
 
-    return (
-        <TableFooter>
-            <TableRow>
-                <TableCell colSpan={1}>
-                    <FormField
-                        control={control}
-                        render={({field}) => (
-                            <FormItem>
-                                <FormControl>
-                                    <Input placeholder="New category name" {...field} />
-                                </FormControl>
-                                <FormMessage/>
-                            </FormItem>
-                        )}
-                        name="name"
-                    />
-                </TableCell>
-                <TableCell colSpan={1}>
-                    <div className="flex flex-grow justify-end">
-                        <Button variant="ghost" type="submit">
-                            + Add category
-                        </Button>
-                    </div>
-                </TableCell>
-            </TableRow>
-        </TableFooter>
-    );
+  return (
+    <TableFooter>
+      <TableRow>
+        <TableCell colSpan={1}>
+          <FormField
+            control={control}
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="New category name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+            name="name"
+          />
+        </TableCell>
+        <TableCell colSpan={1}>
+          <div className="flex flex-grow justify-end">
+            <Button variant="ghost" type="submit">
+              + Add category
+            </Button>
+          </div>
+        </TableCell>
+      </TableRow>
+    </TableFooter>
+  );
 };
